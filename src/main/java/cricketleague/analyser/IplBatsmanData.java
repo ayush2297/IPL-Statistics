61402c74bee24f7ef98d4ee20afea749c4b5e4ae
ayush2297 <ayush.saraf47@gmail.com>
2019-12-14 15:23:33 +0530

    added: POJO to bind csv data

--- src/main/java/cricketleague/analyser/IplBatsmanData.java
+++ src/main/java/cricketleague/analyser/IplBatsmanData.java
@@ -4,4 +4,25 @@ import com.opencsv.bean.CsvBindByName;
 
 public class IplBatsmanData {
 
+    @CsvBindByName (column = "PLAYER", required = true)
+    public String playerName;
+
+    @CsvBindByName (column = "Runs", required = true)
+    public String runsScored;
+
+    @CsvBindByName (column = "HS", required = true)
+    public String highScore;
+
+    @CsvBindByName (column = "Avg", required = true)
+    public String averageScore;
+
+    @CsvBindByName (column = "SR", required = true)
+    public String strikeRate;
+
+    @CsvBindByName (column = "100", required = true)
+    public String hundreds;
+
+    @CsvBindByName (column = "6s", required = true)
+    public String sixes;
+
 }
