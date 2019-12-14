55046d3649e3f3b27d29b51ea68fee0760bf29ec
ayush2297 <ayush.saraf47@gmail.com>
2019-12-14 12:12:18 +0530

    added: class that performs analysis on csv file

--- src/main/java/cricketleague/analyser/CricketLeagueAnalyser.java
+++ src/main/java/cricketleague/analyser/CricketLeagueAnalyser.java
@@ -1,7 +1,17 @@
 package cricketleague.analyser;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class CricketLeagueAnalyser {
     public int loadDataFromCsv(String csvFilePath) {
-        return 0;
+        try (Stream<String> stream = Files.lines(Paths.get(csvFilePath))) {
+            return (int) stream.count();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 }
