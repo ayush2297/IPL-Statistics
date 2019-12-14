80e904e173798a79eba97b0d4f123ad6e5ae98c9
ayush2297 <ayush.saraf47@gmail.com>
2019-12-14 15:13:36 +0530

    added: sample csv file containing few records

--- src/test/java/cricketleague/analyser/CricketLeagueAnalyserTest.java
+++ src/test/java/cricketleague/analyser/CricketLeagueAnalyserTest.java
@@ -4,12 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class CricketLeagueAnalyserTest {
-    public static final String BATTING_CSV = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
+    public static final String BATTING_CSV = "./src/test/resources/battingSample.csv";
+
     @Test
     public void givenCricketLeagueCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() {
         CricketLeagueAnalyser leagueAnalyser = new CricketLeagueAnalyser();
         int numberOfRecords = leagueAnalyser.loadDataFromCsv(BATTING_CSV);
-        Assert.assertEquals(102,numberOfRecords);
+        Assert.assertEquals(8,numberOfRecords);
     }
 
 }
