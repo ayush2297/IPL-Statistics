e8e7427e0b3c080950d7fba1aa3ded4f84c69fbd
ayush2297 <ayush.saraf47@gmail.com>
2019-12-16 11:22:33 +0530

    added: encoding to first field

--- src/main/java/cricketleague/analyser/IplBatsmanData.java
+++ src/main/java/cricketleague/analyser/IplBatsmanData.java
@@ -4,8 +4,21 @@ import com.opencsv.bean.CsvBindByName;
 
 public class IplBatsmanData {
 
+    @CsvBindByName (column = "\uFEFFPOS", required = true)
+    public int position;
+
     @CsvBindByName (column = "PLAYER", required = true)
     public String playerName;
+
+    @CsvBindByName (column = "Mat", required = true)
+    public int matchesPlayed;
+
+
+    @CsvBindByName (column = "Inns", required = true)
+    public int inningsPlayed;
+
+    @CsvBindByName (column = "NO", required = true)
+    public int notOuts;
 
     @CsvBindByName (column = "Runs", required = true)
     public int runsScored;
@@ -16,13 +29,21 @@ public class IplBatsmanData {
     @CsvBindByName (column = "Avg", required = true)
     public Double averageScore;
 
+    @CsvBindByName (column = "BF", required = true)
+    public Double ballsFaced;
+
     @CsvBindByName (column = "SR", required = true)
     public double strikeRate;
 
     @CsvBindByName (column = "100", required = true)
     public int hundreds;
 
+    @CsvBindByName (column = "50", required = true)
+    public int fifties;
+
+    @CsvBindByName (column = "4s", required = true)
+    public int fours;
+
     @CsvBindByName (column = "6s", required = true)
     public int sixes;
-
 }
